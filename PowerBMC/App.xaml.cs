fff2abe299778236d2cf904e264c8aeca3071f28
marqzosvaldo <crosser.xr@gmail.com>
2024-03-29 21:31:14 -0600

    init

---
+++ PowerBMC/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerBMC {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
